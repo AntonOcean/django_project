23b3c1e82ade36d6e8974cbbbff212301f87972b
AntonOcean <tmath90@yandex.ru>
2019-10-29 01:12:48 +0300

    add metriks description and start nginx in local_deploy

--- local_deploy.sh
+++ local_deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # grafana 127.0.0.1:9090 - server mode
+# avg by (instance) (irate(process_cpu_seconds_total{job="django"}[1m]))
+# https://www.robustperception.io/understanding-machine-cpu-usage
+# https://prometheus.io/docs/guides/node-exporter/
+
+sudo sytemctl start nginx
 
 docker stop django_prod-1
 docker stop django_prod-2
@@ -16,4 +21,4 @@ docker stop django_prod_prometheus
 docker build -t django_prod_prometheus .
 docker run --rm --network host --name django_prod_prometheus django_prod_prometheus &
 
-docker run --rm --network host --name django_prod_grafana grafana/grafana
+docker run --rm --network host --name django_prod_grafana grafana/grafana &
